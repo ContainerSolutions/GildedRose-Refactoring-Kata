86c2e60846cb09a43eb34c054ce8b09738b8e083
rmartinez <rmartinez@localhost.localdomain>
2020-11-05 19:28:26 -0300

    refactor quality<50

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -8,25 +8,20 @@ type Item struct {
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 		if items[i].quality < 50 {
-
-			if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-				if items[i].quality > 0 {
-					if items[i].name != "Sulfuras, Hand of Ragnaros" {
-						items[i].quality = items[i].quality - 1
-					}
-				}
-			} else {
-
+			if items[i].name == "Aged Brie" {
 				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						items[i].quality = items[i].quality + 1
-					}
-					if items[i].sellIn < 6 {
-						items[i].quality = items[i].quality + 1
-					}
+			} else if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].sellIn < 6 {
+					items[i].quality = items[i].quality + 3
+				} else if items[i].sellIn < 11 {
+					items[i].quality = items[i].quality + 2
+				} else {
+					items[i].quality = items[i].quality + 1
 				}
-
+			} else if items[i].quality > 0 {
+				if items[i].name != "Sulfuras, Hand of Ragnaros" {
+					items[i].quality = items[i].quality - 1
+				}
 			}
 		}
 
@@ -44,22 +39,6 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		// 	if items[i].name != "Aged Brie" {
-		// 		if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-		// 			if items[i].quality > 0 {
-		// 				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-		// 					items[i].quality = items[i].quality - 1
-		// 				}
-		// 			}
-		// 		} else {
-		// 			items[i].quality = items[i].quality - items[i].quality
-		// 		}
-		// 	} else {
-		// 		if items[i].quality < 50 {
-		// 			items[i].quality = items[i].quality + 1
-		// 		}
-		// 	}
-		// }
 	}
 
 }
