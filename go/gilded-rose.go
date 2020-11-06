ad6330b208a2acf220db875193bee5fb27c257ac
Adam Otto <adamotto89@gmail.com>
2020-11-06 14:11:53 +0100

    move quality change logic to separate function

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -8,46 +8,13 @@ type Item struct {
 }
 
 func UpdateQuality(items []*Item) {
-	var qualityChange int
-LOOP:
 	for _, item := range items {
-		switch n := item.name; {
-		case strings.HasPrefix(n, "Aged Brie"):
-			qualityChange = 1
-		case strings.HasPrefix(n, "Sulfuras"):
-			continue LOOP
-		case strings.HasPrefix(n, "Backstage passes"):
-			if item.sellIn <= 0 {
-				qualityChange = -item.quality
-				break
-			}
-			if item.sellIn <= 5 {
-				qualityChange = 3
-				break
-			}
-			if item.sellIn <= 10 {
-				qualityChange = 2
-				break
-			}
-			qualityChange = 1
-
-		case strings.HasPrefix(n, "Conjured"):
-			if item.sellIn > 0 {
-				qualityChange = -2
-			} else {
-				qualityChange = -4
-			}
-		default:
-			if item.sellIn > 0 {
-				qualityChange = -1
-			} else {
-				qualityChange = -2
-			}
+		qualityChange := defineQualityChange(item)
+		if qualityChange == 0 {
+			continue
 		}
-
 		item.sellIn--
 		item.quality = item.quality + qualityChange
-
 		if item.quality < 0 {
 			item.quality = 0
 		}
@@ -56,4 +23,40 @@ LOOP:
 			item.quality = 50
 		}
 	}
+}
+
+func defineQualityChange(item *Item) (qualityChange int) {
+	switch n := item.name; {
+	case strings.HasPrefix(n, "Aged Brie"):
+		qualityChange = 1
+	case strings.HasPrefix(n, "Sulfuras"):
+		qualityChange = 0
+	case strings.HasPrefix(n, "Backstage passes"):
+		if item.sellIn <= 0 {
+			qualityChange = -item.quality
+			break
+		}
+		if item.sellIn <= 5 {
+			qualityChange = 3
+			break
+		}
+		if item.sellIn <= 10 {
+			qualityChange = 2
+			break
+		}
+		qualityChange = 1
+	case strings.HasPrefix(n, "Conjured"):
+		if item.sellIn > 0 {
+			qualityChange = -2
+		} else {
+			qualityChange = -4
+		}
+	default:
+		if item.sellIn > 0 {
+			qualityChange = -1
+		} else {
+			qualityChange = -2
+		}
+	}
+	return qualityChange
 }
