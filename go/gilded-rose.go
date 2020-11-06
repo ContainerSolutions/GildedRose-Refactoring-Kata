6e959ea47c1618bd25da259dc9f87199e11d03be
rmartinez <rmartinez@localhost.localdomain>
2020-11-06 16:22:36 -0300

    adds IncreaseQualityByOne func

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -9,25 +9,20 @@ func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
 		if items[i].name == "Aged Brie" {
-			if items[i].quality < 50 {
-				if items[i].sellIn > 0 {
-					items[i].quality = items[i].quality + 1
-				} else {
-					items[i].quality = items[i].quality + 2
-				}
+			IncreaseQualityByOne(items[i])
+			if items[i].sellIn <= 0 {
+				IncreaseQualityByOne(items[i])
 			}
 		} else if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].sellIn > 0 {
-				if items[i].quality < 50 {
-
-					items[i].quality = items[i].quality + 1
-					if items[i].sellIn < 6 && items[i].quality < 50 {
-						items[i].quality = items[i].quality + 1
-					}
-					if items[i].sellIn < 11 && items[i].quality < 50 {
-						items[i].quality = items[i].quality + 1
-					}
+				IncreaseQualityByOne(items[i])
+				if items[i].sellIn < 6 {
+					IncreaseQualityByOne(items[i])
 				}
+				if items[i].sellIn < 11 {
+					IncreaseQualityByOne(items[i])
+				}
+
 			} else {
 				items[i].quality = 0
 			}
@@ -46,4 +41,10 @@ func UpdateQuality(items []*Item) {
 
 	}
 
+}
+
+func IncreaseQualityByOne(item *Item) {
+	if item.quality < 50 {
+		item.quality = item.quality + 1
+	}
 }
