cd9b1da44fb8e0e2da7234c8b344e2d423f0836b
Adam Otto <adamotto89@gmail.com>
2020-11-06 12:03:19 +0100

    refactored UpdateQuality method

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -1,58 +1,59 @@
 package main
 
+import "strings"
+
 type Item struct {
 	name            string
 	sellIn, quality int
 }
 
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
+	var qualityChange int
+LOOP:
+	for _, item := range items {
+		switch n := item.name; {
+		case strings.HasPrefix(n, "Aged Brie"):
+			qualityChange = 1
+		case strings.HasPrefix(n, "Sulfuras"):
+			continue LOOP
+		case strings.HasPrefix(n, "Backstage passes"):
+			if item.sellIn <= 0 {
+				qualityChange = -item.quality
+				break
 			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
+			if item.sellIn <= 5 {
+				qualityChange = 3
+				break
 			}
-		}
+			if item.sellIn <= 10 {
+				qualityChange = 2
+				break
+			}
+			qualityChange = 1
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
+		case strings.HasPrefix(n, "Conjured"):
+			if item.sellIn > 0 {
+				qualityChange = -2
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
+				qualityChange = -4
 			}
+		default:
+			if item.sellIn > 0 {
+				qualityChange = -1
+			} else {
+				qualityChange = -2
+			}
+		}
+
+		item.sellIn--
+		item.quality = item.quality + qualityChange
+
+		if item.quality < 0 {
+			item.quality = 0
+		}
+
+		if item.quality > 50 {
+			item.quality = 50
 		}
 	}
-
 }
