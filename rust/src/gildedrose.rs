ae47cbe3872bc2354890fee36c52602a12a85414
rmartinez <rmartinez@localhost.localdomain>
2020-12-15 16:23:58 -0300

    refactor update_quality

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -30,56 +30,52 @@ impl GildedRose {
         GildedRose { items }
     }
 
+    pub fn increase_by_one(item: &mut Item) {
+        if item.quality < 50 {
+            item.quality = item.quality + 1
+        }
+    }
+
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-            {
-                if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality = item.quality - 1;
+            
+            match item.name.as_str() {
+               "Sulfuras, Hand of Ragnaros" => 
+                (),
+               "Aged Brie" =>
+                {
+                    GildedRose::increase_by_one(item);
+                    if item.sell_in <= 0 {
+                       GildedRose::increase_by_one(item);
                     }
-                }
-            } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1;
-
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
+                },
+               "Backstage passes to a TAFKAL80ETC concert" =>
+               {
+                    if item.sell_in > 0 {
+                       GildedRose::increase_by_one(item);
                         if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
+                           GildedRose::increase_by_one(item);
                         }
+                        if item.sell_in < 11 {
+                           GildedRose::increase_by_one(item);
+                        }
+                    } else {
+                        item.quality = 0
                     }
-                }
+               },                                  
+               _ => {
+                    if item.sell_in > 0 {
+                        item.quality = item.quality - 1
+                    } else {
+                        item.quality = 0
+                    }
+               }
             }
 
             if item.name != "Sulfuras, Hand of Ragnaros" {
                 item.sell_in = item.sell_in - 1;
             }
 
-            if item.sell_in < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
         }
     }
 }
